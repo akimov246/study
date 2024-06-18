7f865676de21f7e543f386ab3ceeed87438c39db
akimov246 <akimov246@yandex.ru>
2024-06-19 01:32:30 +0700

    728

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
-from typing import NamedTuple
+import asyncio
+import time
 
-# User = NamedTuple('User', id = int, name = str)
-#
-# user = User(1, 'akimov246')
-# print(user)
+from string import ascii_uppercase
 
-class User(NamedTuple):
-    id: int
-    name: str
+async def coro1():
+    for i in range(10):
+        print(i)
+        await asyncio.sleep(1)
+    return 'Return от coro1'
 
-user = User(2, 'akimoff246')
-print(user)
+async def coro2():
+    for char in ascii_uppercase:
+        print(char)
+        await asyncio.sleep(0.5)
+    return 'Return от coro2'
+
+async def main():
+    to_do = [coro1(), coro2()]
+    for coro in asyncio.as_completed(to_do):
+        res = await coro
+        print(res)
+
+
+asyncio.run(main())
