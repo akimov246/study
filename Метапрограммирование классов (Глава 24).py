1dd478b73460ff36b9f0a01ef196ccac0e802bc4
akimov246 <akimov246@yandex.ru>
2024-07-30 10:29:53 +0700

    846

--- Метапрограммирование классов (Глава 24).py
+++ Метапрограммирование классов (Глава 24).py
@@ -54,3 +54,70 @@ rex = Dog('Rex', 30, 'Bob')
 print(rex)
 
 # Введение в __init_subclass__
+
+from collections.abc import Callable
+from typing import Any, NoReturn, get_type_hints
+
+class Field:
+    def __init__(self, name: str, constructor: Callable) -> None:
+        if not callable(constructor) or constructor is type(None):
+            raise TypeError(f'{name} type hint must be callable')
+        self.name = name
+        self.constructor = constructor
+
+    def __set__(self, instance: Any, value: Any) -> None:
+        if value is ...: # Если Checked.__init__ присваивает value значение ... (встроенный объект Ellipsis), то мы
+                         # вызываем constructor без аргументов
+            value = self.constructor()
+        else:
+            try:
+                value = self.constructor(value) # В противном случае вызываем constructor с заданным значением value
+            except (TypeError, ValueError) as e: # Если constructor возбуждает одно из этих исключений, то возбуждаем
+                                                 # TypeError с содержательным сообщением
+                type_name = self.constructor.__name__
+                msg = f'{value} is not compatible with {self.name}:{type_name}'
+                raise TypeError(msg) from e
+            instance.__dict__[self.name] = value # Если исключений не было, то value сохраняется в instance.__dict__
+
+class Checked:
+    @classmethod
+    def _fields(cls) -> dict[str, type]:
+        return get_type_hints(cls)
+
+    def __init_subclass__(subclass) -> None:
+        super().__init_subclass__()
+        for name, constructor in subclass._fields().items():
+            setattr(subclass, name, Field(name, constructor))
+
+    def __init__(self, **kwargs: Any) -> None:
+        for name in self._fields():
+            value = kwargs.pop(name, ...)
+            setattr(self, name, value)
+        if kwargs:
+            self.__flag_unknown_attrs(*kwargs)
+
+    def __setattr__(self, name: str, value: Any) -> None: # Перехватывать все попытки установить атрибут экземпляра.
+                                                          # Необходимо, чтобы предотвратить присваивание неизвестному атрибуту
+        if name in self._fields(): # Если атрибут с именем name известен, получить соответствующий дескриптор.
+            cls = self.__class__
+            descriptor = getattr(cls, name)
+            descriptor.__set__(self, value)
+        else:
+            self.__flag_unknown_attrs(name)
+
+    def __flag_unknown_attrs(self, *names: str) -> NoReturn:
+        plural = 's' if len(names) > 1 else ''
+        extra = ', '.join(f'{name}' for name in names)
+        cls_name = repr(self.__class__.__name__)
+        raise AttributeError(f'{cls_name} object has no attribute{plural} {extra}')
+
+    def _asdict(self) -> dict[str, Any]:
+        return {
+            name: getattr(self, name)
+            for name, attr in self.__class__.__dict___.items()
+            if isinstance(attr, Field)
+        }
+
+    def __repr__(self) -> str:
+        kwargs = ', '.join(f'{key}={value}' for key, value in self._asdict().items())
+        return f'{self.__class__.__name__}({kwargs})'
